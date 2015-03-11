4871c7b9c05bdfa2cdc03f28aed59c6e97a12d19
Jared Beckham <jtbeckha@gmail.com>
2015-03-11 08:01:24 -0400

    update scaling

--- boot.js
+++ boot.js
@@ -3,15 +3,17 @@ var boot = function(game) {};
 
 boot.prototype = {
     create: function() {
-        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.minWidth = 480;
-        this.scale.minHeight = 260;
-        this.scale.maxWidth = 1024;
-        this.scale.maxHeight = 768;
-        this.scale.forceLandscape = true;
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //resizes game to fit parent but maintains aspect ratio
+        this.scale.setMinMax(480,260,1024,768);
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
-        this.scale.setScreenSize(true); //may need to remove/change this since method is deprecated
+        //these functions can be used to force users to use a specific orientation on mobile devices
+        /*
+        if (this.game.device.desktop === false) {
+            this.scale.forceOrientation(true,false);
+            this.scale.enterIncorrectOrientation.add(function () {}, this);
+            this.scale.leaveIncorrectOrientation.add(function() {}, this);
+         }*/
         //this is where we can load a wrapper menu instead of the preload state of first game
         this.game.state.start("Wrapper");
     }
