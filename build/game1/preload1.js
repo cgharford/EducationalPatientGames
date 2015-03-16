fe67ede61e3bf0b0e8fc357937182c02ea82d07f
Devin Beauchamp <devmariebea@dhcp-v880-9-00063.1Xwireless.unc.edu>
2015-03-15 21:57:07 -0400

    organized file system

--- build/game1/preload1.js
+++ build/game1/preload1.js
@@ -3,9 +3,9 @@ var preload1 = function(game) {};
 
 preload1.prototype = {
     preload: function() {
-        this.game.load.image('safe','./game1/assets/bluesquare.png');
-        this.game.load.image('unsafe','./game1/assets/redsquare.png'); //todo:  load these as spritesheets and add animtions
-        this.game.load.image('play button', './game1/assets/button1.png');
+        this.game.load.image('safe','./assets/images/bluesquare.png');
+        this.game.load.image('unsafe','./assets/images/redsquare.png'); //todo:  load these as spritesheets and add animtions
+        this.game.load.image('play button', './assets/images/button1.png');
     },
     create: function() {
         this.game.state.add("Title1",title1);
