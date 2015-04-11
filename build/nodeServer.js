66d39f9dcfdf74c4359586aa41b080299f100dbd
Jared Beckham <jtbeckha@gmail.com>
2015-04-11 15:00:55 -0400

    able to add new field to mongo

--- build/nodeServer.js
+++ build/nodeServer.js
@@ -1,9 +1,15 @@
-
-
+//mongo runs on port 27017, our database is called educational_patient_games and the collection for
+//captain safety is captain_safety
 
 var http = require("http");
 var qs = require("querystring");
 var port = process.argv[2];  //pass the port number as first argument in the command line
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var DATABASENAME = "educational_patient_games";
+var COLLECTIONNAME = "captain_safety";
+var url = "mongodb://localhost:27017/" + DATABASENAME;
+
 var server = http.createServer(function(request, response) {
     //POST - create
     //GET - read
@@ -12,6 +18,7 @@ var server = http.createServer(function(request, response) {
     var body = '';
     if (request.method === "POST") {
         //create a new record
+        //post should just create a record and set score to 0
         request.on('data', function(data) {
             body += data;
             if (body.length > 1e6) {
@@ -20,8 +27,19 @@ var server = http.createServer(function(request, response) {
         });
         request.on('end', function() {
             var obj = JSON.parse(body);
-            console.log(obj.username);
-            console.log(obj.score);
+            console.log("creating new record in database");
+            console.log("username of " + obj.username); //this is how you
+            console.log("score of " + obj.score);
+            console.log(obj);
+            MongoClient.connect(url, function (err, db) {
+                if (err) {
+                    //stop the function
+                }
+                console.log("connected to mongo, database: + " + DATABASENAME);
+                var collection = db.collection(COLLECTIONNAME);
+                insertDocuments(collection, obj);
+                db.close();
+            });
         })
     }
     if (request.method === "GET") {
@@ -47,3 +65,10 @@ var server = http.createServer(function(request, response) {
     });
 server.listen(port);
 console.log("Server ready");
+
+function insertDocuments(collection, data) {
+    collection.insertOne(data, function(err, r) {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+    })
+}
