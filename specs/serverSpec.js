41085417263fabdcc98d0f970da681afa674a8ba
Devin <dbeaucha@live.unc.edu>
2015-04-13 18:11:46 -0400

    upload test file, since it got deleted before

--- specs/serverSpec.js
+++ specs/serverSpec.js
@@ -1,17 +1,24 @@
 
-
-//describe is a suite
-//it("test", function() {}) is a test
-//expect is the jasmine equivalent of assert
-
-"use strict";
-
-describe("Hello World", function() {
-    //myModule = require("example.js");
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello World!");
-    });
-    it("is not null", function() {
-        expect(helloWorld()).not.toEqual(null);
-    })
+var http = require("http");
+var port = process.argv[2];  //pass the port number as first argument in the command line
+var server = http.createServer(function(request, response) {
+    //POST - create
+    //GET - read
+    //PUT - update
+    //DELETE - delete
+    if (request.method === "POST") {
+        //create a new record
+    }
+    if (request.method === "GET") {
+        //read a record
+    }
+    if (request.method === "PUT") {
+        //update existing record
+    }
+    if (request.method === "DELETE") {
+        //delete a record
+    }
+    response.end("Hello");
 });
+server.listen(port);
+console.log("Server ready");
---
+++ specs/test_one.js
@@ -0,0 +1,62 @@
+describe("Educational Patient Games", function(){
+    describe("Wrapper loads", function(){
+        it("should preload game images", function(){
+            //expect().tobe();
+        });
+
+        it("should add game images", function(){
+            //expect().tobe();
+        });
+
+    });
+    describe("Choose game to play", function(){
+        it("should play Captain Safety on click", function(){
+            //expect().tobe();
+        });
+
+    });
+    describe("Captain Safety loads", function(){
+        it("should preload game image", function(){
+            //expect().tobe();
+        });
+        it("should add game image", function(){
+            //expect().tobe();
+        });
+
+    });
+    describe("Play Captain Safety", function(){
+        it("should load game on play button click", function(){
+            //expect().tobe();
+        });
+    });
+
+    describe("Captain Safety game play", function(){
+
+        describe("Timer updates", function(){
+            it("should continuously decrement from 60 to 0", function(){
+                //expect().tobe();
+            });
+        });
+        describe("Click safe children", function(){
+            it("should play bad sound", function(){
+                //expect().tobe();
+            });
+            it("should decrement score", function(){
+                //expect().tobe();
+            });
+        });
+        describe("Click unsafe children", function(){
+            it("should increment score", function(){
+                //expect().tobe();
+            });
+        });
+        describe("On victory", function(){
+            it("should have -1 on timer", function(){
+                //expect().tobe();
+            });
+            it("should load victory page", function(){
+                //expect().tobe();
+            });
+        });
+    });
+});
