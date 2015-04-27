2e7525a1816952e101163f808ba10fac930a6f22
Devin <dbeaucha@live.unc.edu>
2015-04-27 00:03:26 -0400

    wrapper, commented out password

--- build/wrapper.js
+++ build/wrapper.js
@@ -5,29 +5,35 @@ var wrapper = function(game) {};
 wrapper.prototype = {
     preload: function() {
 	    //load images for all of the games here
-        this.game.load.image("cs", "assets/images/UIP-thumbnail.jpg");
-        this.game.load.image("ng", "assets/images/newgame.png");
+        this.game.load.image("wrapper-bg", "assets/images/wrapper.jpg");
+        this.game.load.image("new-game-thumb", "assets/images/new-game-thumbnail.jpg");
+        this.game.load.image("UIP-thumb", "assets/images/UIP-thumbnail.jpg");
     },
     create: function() {
-        var textStyle = textStyle = {font: "50px Arial", fill: "#ffffff", align: "center", wordWrap: false};
-        var instructionText = this.game.add.text(this.game.width/2,0,"Select a game to play", textStyle);
-        instructionText.anchor.set(0.5,0);
+        
+        wrapperBg = this.add.sprite(1024, 768, 'wrapper-bg');
+        wrapperBg.x = 0;
+        wrapperBg.y = 0;
+        wrapperBg.height = this.game.height;
+        wrapperBg.width = this.game.width;
 
-        var csImage = this.game.add.sprite(this.game.width/6, this.game.height/2, "cs");
-        csImage.anchor.set(.5);
-        csImage.scale.setTo(.8,.8);
-        csImage.inputEnabled = true;
-        csImage.events.onInputDown.add(this.captainSafetySelect,this);
 
-        var ngImageA = this.game.add.sprite(this.game.width/2, this.game.height/2, "ng");
+        var uipImage = this.game.add.sprite(this.game.width/2, this.game.height/2, "UIP-thumb");
+        uipImage.anchor.set(.5);
+        //uipImage.scale.setTo(.8,.8);
+        uipImage.inputEnabled = true;
+        uipImage.events.onInputDown.add(this.captainSafetySelect,this);
+
+        var ngImageA = this.game.add.sprite(this.game.width/6, this.game.height/2, "new-game-thumb");
         ngImageA.anchor.set(.5);
-        ngImageA.scale.setTo(.8,.8);
+        //ngImageA.scale.setTo(.8,.8);
 
-        var ngImageB = this.game.add.sprite(5*this.game.width/6, this.game.height/2, "ng");
+
+        var ngImageB = this.game.add.sprite(5*this.game.width/6, this.game.height/2, "new-game-thumb");
         ngImageB.anchor.set(.5);
-        ngImageB.scale.setTo(.8,.8);
+        //ngImageB.scale.setTo(.8,.8);
 
-        if(csImage !== null && ngImageA !== null && ngImageB !== null){
+        if(uipImage !== null && ngImageA !== null && ngImageB !== null){
             return true;
         } else {
             return false;
