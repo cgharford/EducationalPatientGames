855ac9b0319837a9e05107ad612a891c8942bc0e
clhlms <clhelms@live.unc.edu>
2015-05-02 23:45:42 -0400

    unit tests

--- specs/test_one.js
+++ specs/test_one.js
@@ -1,97 +1,166 @@
-describe("Accessing Phaser", function() {
-    it("should be defined", function() {
-        var game = new Phaser.Game();
-        var gameFrame = new game1(game);
-        expect(gameFrame).toBeDefined();
-    }),
+describe("Educational Patient Games", function() {
+    describe("Accessing Phaser and the Game", function() {
+        var game;
+        var w;
+        var h;
 
-        it("phaser height", function() {
-            var game = new Phaser.Game();
-            expect(game.height).toBe(600)
-        }),
+        beforeEach(function() {
+            w = window.innerWidth * window.devicePixelRatio;
+            h = window.innerHeight * window.devicePixelRatio;
+            game = new Phaser.Game((h > w) ? h : w, (h > w) ? w : h,Phaser.CANVAS, "game-container");
+        });
 
-        it("test", function() {
-            var game = new Phaser.Game();
-            var gameFrame = new game1(game);
-        }),
+        it("Phaser should be defined", function() {
+            expect(game).toBeDefined();
+        });
 
-        it("functions should be defined", function() {
-            var game = new Phaser.Game();
-            var gameObject = new game1(game);
-            expect(gameObject.create).toBeDefined();
-        })
-});
-
-describe("Test One", function(){
-    it("should say hello world", function(){
-        expect(helloWorld()).toBe("Hello World!");
+        it("Phaser parent is 'game-container'", function() {
+            expect(game.parent).toBe("game-container");
+        });
     });
 
-    it("should not be null", function(){
-        expect(helloWorld()).not.toBe(null);
+    describe("Game loads", function() {
+        var w;
+        var h;
+        var game;
+        var gameObject;
+
+
+        beforeEach(function() {
+            w = window.innerWidth * window.devicePixelRatio;
+            h = window.innerHeight * window.devicePixelRatio;
+            game = new Phaser.Game((h > w) ? h : w, (h > w) ? w : h,Phaser.CANVAS, "game-container");
+            gameObject = new game1(game);
+        });
+
+        it("Game is defined", function() {
+            expect(gameObject).toBeDefined();
+        });
+
+        describe("Expect essential functions to be defined", function() {
+
+            it("Game create function is defined", function () {
+                expect(gameObject.create).toBeDefined();
+            });
+
+            it("Game update function is defined", function () {
+                expect(gameObject.update).toBeDefined();
+            });
+
+        });
+
+        it("Time Remaining is 59", function() {
+            gameObject.timeRemaining = 60;
+            gameObject.updateTime();
+            //gameObject.pauseGame();
+            expect(gameObject.timeRemaining).toEqual(59);
+        });
     });
-});
 
-
-describe("Educational Patient Games", function(){
     describe("Wrapper loads", function(){
-        it("should preload game images", function(){
-            //expect().toBe(true);
+        var game;
+        var wrapObject;
+
+        beforeEach(function() {
+            game = new Phaser.Game();
+            wrapObject = new wrapper(game);
         });
 
-        it("should add game images", function(){
-            //expect().tobe();
+        it("Wrapper is defined", function(){
+            expect(wrapObject).toBeDefined();
+        });
+
+        describe("Wrapper essential functions are defined", function() {
+            it("Wrapper preload function is defined", function() {
+                expect(wrapObject.preload).toBeDefined();
+            });
+
+            it("Wrapper create function is defined", function() {
+                expect(wrapObject.create).toBeDefined();
+            })
+        });
+
+        it("Uploads all images in wrapper", function() {
+            expect(wrapObject.create).toBeTruthy();
         });
 
     });
-    describe("Choose game to play", function(){
-        it("should play Captain Safety on click", function(){
-            //expect().tobe();
+
+    describe("Preload loads", function (){
+        var game;
+        var preloadObject;
+
+        beforeEach( function() {
+            game = new Phaser.Game();
+            preloadObject = new preload1(game);
         });
 
+        it("Preload is defined", function() {
+            expect(preloadObject).toBeDefined();
+        });
+
+        describe("Preload essential functions are defined", function() {
+            it("Preload preload function is defined", function() {
+                expect(preloadObject.preload).toBeDefined();
+            });
+
+            it("Preload create function is defined", function() {
+                expect(preloadObject.create).toBeDefined();
+            })
+        });
     });
-    describe("Captain Safety loads", function(){
-        it("should preload game image", function(){
-            //expect().tobe();
-        });
-        it("should add game image", function(){
-            //expect().tobe();
+
+    describe("Title loads", function() {
+        var game;
+        var titleObject;
+
+        beforeEach( function() {
+            game = new Phaser.Game();
+            titleObject = new title1(game);
         });
 
+        it("Title is defined", function() {
+            expect(titleObject).toBeDefined();
+        });
+
+        it("Title create function is defined", function() {
+            expect(titleObject.create).toBeDefined();
+        });
     });
-    describe("Play Captain Safety", function(){
-        it("should load game on play button click", function(){
-            //expect().tobe();
+
+    describe("Victory loads", function() {
+        var game;
+        var victoryObject;
+
+        beforeEach(function() {
+            game = new Phaser.Game();
+            victoryObject = new victory1(game, 10);
+        });
+
+        it("Victory is defined", function() {
+            expect(victoryObject).toBeDefined();
+        });
+
+        it("Victory create function is defined", function() {
+            expect(victoryObject.create).toBeDefined();
         });
     });
 
-    describe("Captain Safety game play", function(){
+    describe("Boot loads", function() {
+        var game;
+        var bootObject;
 
-        describe("Timer updates", function(){
-            it("should continuously decrement from 60 to 0", function(){
-                //expect().tobe();
-            });
+        beforeEach(function() {
+            game = new Phaser.Game();
+            bootObject = new boot(game);
         });
-        describe("Click safe children", function(){
-            it("should play bad sound", function(){
-                //expect().tobe();
-            });
-            it("should decrement score", function(){
-                //expect().tobe();
-            });
+
+        it("Boot is defined", function() {
+            expect(bootObject).toBeDefined();
         });
-        describe("Click unsafe children", function(){
-            it("should increment score", function(){
-                //expect().tobe();
-            });
-        });
-        describe("On victory", function(){
-            it("should have -1 on timer", function(){
-                //expect().tobe();
-            });
-            it("should load victory page", function(){
-                //expect().tobe();
-            });
+
+        it("Boot create function is defined", function() {
+            expect(bootObject.create).toBeDefined();
         });
     });
 });
