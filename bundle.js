47c8fc8570ec2c8da00841b106cdd5c1809c0b46
Deven Desai <drdesai@cs.unc.edu>
2015-11-04 12:47:58 -0500

    Fixed Score Bug

--- bundle.js
+++ bundle.js
@@ -850,7 +850,7 @@ module.exports = {
         good_sound = this.add.audio('good_sound');
 
         // Score starts at 0, timer starts at 60 seconds
-        this.score = 0;
+        score = 0;
         timeRemaining = 60;
         maxTime = timeRemaining
         textStyle = {
@@ -861,7 +861,7 @@ module.exports = {
         };
 
         //  Place score and timer in upper left hand corner
-        scoreText = this.game.add.text(500, this.game.width / 50, 'Score: ' + this.score, {
+        scoreText = this.game.add.text(500, this.game.width / 50, 'Score: ' + score, {
             fill: '#FFFFFF'
         });
         clockText = this.game.add.text(700 + scoreText.width, this.game.width / 50, 'Time Remaining: ' + timeRemaining, {
@@ -894,7 +894,7 @@ module.exports = {
         this.game.input.onDown.add(this.unpauseGame, this);
 
         // On time out, show score
-        victoryText = this.game.add.text(this.game.width / 2, this.game.height / 2, 'Congratulations your score is ' + this.score + '!', textStyle);
+        victoryText = this.game.add.text(this.game.width / 2, this.game.height / 2, 'Congratulations your score is ' + score + '!', textStyle);
         victoryText.visible = false;
         victoryText.anchor.set(0.5);
         this.pauseGame();
@@ -944,9 +944,9 @@ module.exports = {
         }
 
         // Update score, timer, and victory texts with new values
-        scoreText.text = 'Score: ' + this.score;
+        scoreText.text = 'Score: ' + score;
         clockText.text = 'Time Remaining: ' + timeRemaining;
-        victoryText.text = 'Congratulations your score is ' + this.score + '!';
+        victoryText.text = 'Congratulations your score is ' + score + '!';
 
         // If timer runs out, show victory
         if (timeRemaining <= 0) {
@@ -982,7 +982,7 @@ module.exports = {
             // Ensure kill of screen sprites
             if (currentChild.position.x > this.game.width) {
                 this.multiplier += 1;
-                this.score += 10 * this.multiplier;
+                score += 10 * this.multiplier;
                 currentChild.kill(); //weird stuff still happening with killing offscreen?
                 safeChildren.remove(currentChild);
             }
@@ -1079,7 +1079,7 @@ module.exports = {
      */
     onUnsafeClick: function(sprite) {
 
-        this.score += 10 * this.multiplier;
+        score += 10 * this.multiplier;
         if (this.multiplier !== 20) {
             this.multiplier++;
         }
@@ -1106,7 +1106,7 @@ module.exports = {
         });
         victoryText.visible = true;
         //change to victory state
-        this.game.state.start("Victory2", true, false, this.score);
+        this.game.state.start("Victory2", true, false, score);
     },
     /**
      * Function for placing random children -- NO LONGER USED BUT MAINTAINED FOR FUTURE
