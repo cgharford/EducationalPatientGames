1b42eb31462bc31557baa5ab242f90d1d510873a
Joshua Potter <jrpotter2112@gmail.com>
2016-04-01 02:04:22 -0400

    timer adjusted

--- games/burn/assets/js/puzzle.js
+++ games/burn/assets/js/puzzle.js
@@ -3,27 +3,41 @@ $(function() {
     // Global reference
     window.puzzle = {};
 
-    // Perform changes to the timer
-    +function() {
+    // Allow selection of the puzzle to work with
+    // We simply take the image in question and remove the other puzzle images
+    // in place of a canvas with the background set to the selected image to
+    // scramble.
+    $('.puzzle-img').click(function() {
+        var that = $(this);
 
-        var minutes = 0;
-        var seconds = 0;
-        var display = $('#timer').find('span');
+        // Do not allow for selection of any other puzzles
+        $('#level-select-container').hide();
 
-        var format = function(value) {
-            var prepend = (value < 10) ? "0" : "";
-            return prepend + value;
-        }
+        // Begin starting the timer now that the puzzle has been selected.
+        +function() {
 
-        window.puzzle.intervalId = setInterval(function() {
-            seconds += 1;
-            if(seconds === 60) {
-                minutes += 1;
-                seconds = 0;
+            var bar = $('#tool-bar').show();
+            var display = bar.find('#timer').find('span');
+
+            var minutes = 0;
+            var seconds = 0;
+            var format = function(value) {
+                var prepend = (value < 10) ? "0" : "";
+                return prepend + value;
             }
-            display.text(format(minutes) + ":" + format(seconds));
-        }, 1000);
 
-    }();
+            window.puzzle.intervalId = setInterval(function() {
+                seconds += 1;
+                if(seconds === 60) {
+                    minutes += 1;
+                    seconds = 0;
+                }
+                display.text(format(minutes) + ":" + format(seconds));
+            }, 1000);
+
+        }();
+    });
+
+
 
 });
