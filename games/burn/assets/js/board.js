0624bea0699a2a475fb3e890dbaa7363ac35205d
Joshua Potter <jrpotter2112@gmail.com>
2016-04-03 17:47:13 -0400

    revising puzzle code

--- games/burn/assets/js/board.js
+++ games/burn/assets/js/board.js
@@ -6,61 +6,271 @@
 // future projects could look into integrating Ruby on Rails or Django with the
 // project (it is possible to do with Microsoft Azure)
 
-+function() {
+/**
+ * Puzzle Piece
+ * ==================================
+ *
+ * Representation of a puzzle piece.
+*/
+var TileTemplate = function(width, height) {
 
-    // TODO: Provide the image randomly server side, not client side
-    // This could perhaps just be a data attribute set on the canvas element
-    // and then pulled up from the following code
+    this.width  = width;
+    this.height = height;
+    this.top    = this.Edge.FLAT;
+    this.right  = this.Edge.FLAT;
+    this.bottom = this.Edge.FLAT;
+    this.left   = this.Edge.FLAT;
+
+    // Get Values
+    // Convenience function for shaping tiles
+    this.getEdges = function() {
+        return [this.top, this.right, this.bottom, this.left];
+    };
+
+    // Create Tile
+    // Goes through and masks the piece, creates the border, and the tile raster
+    this.createTile = function(image, position) {
+
+        // Create Mask
+        var mask = this.createMask();
+
+        // Obtain tile raster
+        var clone = image.clone();
+        var size = new Size(this.width, this.height);
+        var offset = new Point(this.width * position[0], this.height * position[1]);
+        var tileRaster = this.createTileRaster(clone, size, offset);
+
+        // Create border
+        var border = mask.clone();
+        border.strokeColor = '#CCC';
+        border.strokeWidth = 15;
+
+        // Join all components together to form the tile
+        return new Group(mask, border, tileRaster);
+    };
+
+    // Create Mask
     //
-    // var raster_bg = new Raster($('#board').data('src'));
-    var potential_bgs = [
-        'stove-puzzle.jpg'
-    ];
-    var background = potential_bgs[Math.floor(Math.random() * (potential_bgs.length + 1)]);
-    var background = new Raster('assets/imgs/' + background);
+    // The following applies to tile pieces to obtain the look of a puzzle piece.
+    // A path forming to the shape of a puzzle piece (according to the +/-1) value
+    // of the edge is pathed out, and eventually returned to apply to the piece
+    // in question.
+    this.createMask = function() {
 
-    // Define the puzzle piece orientations
-    +function() {
+        var ratio = this.width / 100.0;
+        var curvyCoords = [
+              0, 0, 35, 15, 37, 5,
+              37, 5, 40, 0, 38, -5,
+              38, -5, 20, -20, 50, -20,
+              50, -20, 80, -20, 62, -5,
+              62, -5, 60, 0, 63, 5,
+              63, 5, 65, 15, 100, 0
+        ];
 
-        // The edge indicates how the piece meets with another piece. Ultimately,
-        // it is not possible to place one piece to another if the sum of their
-        // edges do not add to 0
-        var Edge = Object.freeze({
-            FLAT: 0,
-            CONVEX: 1,
-            CONCAVE: -1
-        });
+        var mask = new Path();
+        var tileCenter = view.center;
 
-        // Each puzzle shape is defined as an array of 4 Edge objects
-        // where the indices represent respectively top, right, bottom, left
-        window.puzzle.shapePieces = function(width, height) {
-            var shapes = [];
+        // We build the path around the rectangular piece
+        var topLeftEdge = new Point(-4, 4);
+        mask.moveTo(topLeftEdge);
+        for (var i = 0; i < curvyCoords.length / 6; i++) {
+            var p1 = topLeftEdge + new Point(curvyCoords[i * 6 + 0] * ratio, this.top * curvyCoords[i * 6 + 1] * ratio);
+            var p2 = topLeftEdge + new Point(curvyCoords[i * 6 + 2] * ratio, this.top * curvyCoords[i * 6 + 3] * ratio);
+            var p3 = topLeftEdge + new Point(curvyCoords[i * 6 + 4] * ratio, this.top * curvyCoords[i * 6 + 5] * ratio);
+            mask.cubicCurveTo(p1, p2, p3);
+        }
 
-            for(var i = 0; i < width; i++) {
-                for(var j = 0; j < height; j++) {
+        var topRightEdge = topLeftEdge + new Point(this.width, 0);
+        for (var i = 0; i < curvyCoords.length / 6; i++) {
+            var p1 = topRightEdge + new Point(-this.right * curvyCoords[i * 6 + 1] * ratio, curvyCoords[i * 6 + 0] * ratio);
+            var p2 = topRightEdge + new Point(-this.right * curvyCoords[i * 6 + 3] * ratio, curvyCoords[i * 6 + 2] * ratio);
+            var p3 = topRightEdge + new Point(-this.right * curvyCoords[i * 6 + 5] * ratio, curvyCoords[i * 6 + 4] * ratio);
+            mask.cubicCurveTo(p1, p2, p3);
+        }
 
-                    var currentShape = [];
+        var bottomRightEdge = topRightEdge + new Point(0, this.width);
+        for (var i = 0; i < curvyCoords.length / 6; i++) {
+            var p1 = bottomRightEdge - new Point(curvyCoords[i * 6 + 0] * ratio, this.bottom * curvyCoords[i * 6 + 1] * ratio);
+            var p2 = bottomRightEdge - new Point(curvyCoords[i * 6 + 2] * ratio, this.bottom * curvyCoords[i * 6 + 3] * ratio);
+            var p3 = bottomRightEdge - new Point(curvyCoords[i * 6 + 4] * ratio, this.bottom * curvyCoords[i * 6 + 5] * ratio);
+            mask.cubicCurveTo(p1, p2, p3);
+        }
 
-                    // Top/bottom edges
-                    if(i === 0) {
-                        currentShape[0] = Edge.FLAT;
-                    } else if(i === height - 1) {
-                        currentShape[2] = Edge.FLAT;
-                    }
+        var bottomLeftEdge = bottomRightEdge - new Point(this.width, 0);
+        for (var i = 0; i < curvyCoords.length / 6; i++) {
+            var p1 = bottomLeftEdge - new Point(-this.left * curvyCoords[i * 6 + 1] * ratio, curvyCoords[i * 6 + 0] * ratio);
+            var p2 = bottomLeftEdge - new Point(-this.left * curvyCoords[i * 6 + 3] * ratio, curvyCoords[i * 6 + 2] * ratio);
+            var p3 = bottomLeftEdge - new Point(-this.left * curvyCoords[i * 6 + 5] * ratio, curvyCoords[i * 6 + 4] * ratio);
+            mask.cubicCurveTo(p1, p2, p3);
+        }
 
-                    // Left/right edges
-                    if(j === 0) {
-                        currentShape[3] = Edge.FLAT;
-                    } else if(j === width - 1) {
-                        currentShape[2] = Edge.FLAT;
-                    }
+        mask.opacity = 0.25;
+        mask.strokeColor = '#FFF';
 
-                    shapes.push(currentShape);
+        return mask;
+    };
+
+    // Create Tile Raster
+    //
+    // The following pulls out the segment of the image in question (with some
+    // margin), setting the data of the raster to the segment
+    this.createTileRaster = function(src, size, offset) {
+        var marginWidth = this.width * 0.203125;
+        var tileWithMarginWidth = size.width + marginWidth * 2;
+        var raster = new Raster(document.getElementById('empty'));
+        raster.position = new Point(28, 36);
+        raster.setData(src.getData(
+            new Rectangle(
+                offset.x - marginWidth,
+                offset.y - marginWidth,
+                tileWithMarginWidth,
+                tileWithMarginWidth)),
+            new Point(0, 0));
+        return raster;
+    };
+
+};
+
+// The edge indicates how the piece meets with another piece. It is not possible
+// to place one piece to another if the sum of their edges do not add to 0
+TileTemplate.prototype.Edge = Object.freeze({
+    FLAT: 0,
+    CONVEX: 1,
+    CONCAVE: -1
+});
+
+/**
+ * Puzzle
+ * ==================================
+ *
+ * Representation of the actual puzzle and its current state.
+*/
+var Puzzle = function() {
+
+    // Get Dimension
+    //
+    // Convenience function that returns the dimension of the puzzle according
+    // to the difficulty class. The returned value is of form [width, height].
+    this.getDimension = function() {
+        switch(this.difficulty) {
+            case this.Difficulty.EASY:
+                return [2, 2];
+            case this.Difficulty.MEDIUM:
+                return [3, 3];
+            case this.Difficulty.HARD:
+                return [4, 4];
+            case this.Difficulty.INSANE:
+                return [5, 5];
+            case this.Difficulty.NIGHTMARE:
+                return [6, 6];
+            default:
+                return [1, 1];
+        }
+    };
+
+    // Randomize Edges
+    //
+    // When creating the puzzle, each piece is setup so that each side is
+    // either flat (if on the edge of the puzzle), convex, or concave. These
+    // are represented by the TileTemplate.prototype.Edge enumeration. All adjacent
+    // edges should add to 0 - if all pieces are connected and the sum is 0,
+    // then the puzzle must be completed.
+    //
+    // We first define the outer edges, adding all the pieces and then define
+    // the inner edges.
+    this.randomizeEdges = function() {
+        var outlines = [];
+        var width = this.getDimension()[0];
+        var height = this.getDimension()[1];
+        for(var i = 0; i < width; i++) {
+            for(var j = 0; j < height; j++) {
+                outlines.push(new TileTemplate(this.tileWidth, this.tileHeight));
+            }
+        }
+
+        // Randomize the right and bottom of each piece, and match the corresponding
+        // edge to add to 0
+        for(var i = 0; i < height; i++) {
+            for(var j = 0; j < width; j++) {
+                if(j < width - 1) {
+                    outlines[i * height + j].right = (Math.random() >= 0.5) ? 1 : -1;
+                    outlines[i * height + j + 1].left = -outlines[i * height + j].right;
+                }
+                if(i < height - 1) {
+                    outlines[i * height + j].bottom = (Math.random() >= 0.5) ? 1 : -1;
+                    outlines[(i+1) * height + j].top = -outlines[i * height + j].bottom;
                 }
             }
+        }
 
+        return outlines;
+    };
+
+    // The following function bootstraps the puzzle onto the canvas for
+    // manipulating. In particular, it randomizes the pieces after generating
+    // them, and allows for them to be moved and linked together. Note the
+    // passed @selector is the selector of the canvas object the puzzle should
+    // be placed onto.
+    this.load = function(config) {
+
+        // General Properties
+        this.tiles = [];
+        this.path = config.path;
+        this.difficulty = config.difficulty;
+        this.image = new Raster(config.image);
+        this.image.opacity = 0;
+
+        // Tile Properties
+        var width = this.getDimension()[0];
+        var height = this.getDimension()[1];
+
+        this.tileWidth = this.image.width / width;
+        this.tileHeight = this.image.height / height;
+
+        // Building the tiles and collecting them
+        var templates = this.randomizeEdges();
+        for(var i = 0; i < height; i++) {
+            for(var j = 0; j < width; j++) {
+                var templ = templates[i * width + j];
+                var tile = templ.createTile(this.image, [j, i]);
+
+                tile.opacity = 1;
+                tile.clipped = true;
+                tile.shape = templ.getEdges();
+                tile.imagePosition = new Point(j, i);
+
+                this.tiles.push(tile);
+            }
+        }
+
+        // Position the tiles on the board
+        for(var i = 0; i < height; i++) {
+            for(var j = 0; j < width; j++) {
+                var current = this.tiles[i * width + j];
+                current.position = new Point(0, 0);
+                current.cellPosition = new Point(j + 1, i + 1);
+            }
         }
 
     }
 
-}();
+};
+
+Puzzle.prototype.Difficulty = Object.freeze({
+    EASY: 0,      // 2x2
+    MEDIUM: 1,    // 3x3
+    HARD: 2,      // 4x4
+    INSANE: 3,    // 5x5
+    NIGHTMARE: 4, // 6x6
+});
+
+/**
+ * Global reference
+ * ==================================
+ *
+ * Note that, in order to continue using Paperscript, but still reference the
+ * puzzle object outside of the script, we first run the board.js script and
+ * afterward run puzzle.js, which will bootstrap our choices of puzzle and
+ * difficulty.
+*/
+window.puzzle = new Puzzle();
--- games/burn/assets/js/puzzle.js
+++ games/burn/assets/js/puzzle.js
@@ -1,43 +1,47 @@
 $(function() {
 
-    // Global reference
-    window.puzzle = {};
+    // Begin starting the timer now that the puzzle has been selected.
+    var initializeTimer = function() {
 
-    // Allow selection of the puzzle to work with
-    // We simply take the image in question and remove the other puzzle images
-    // in place of a canvas with the background set to the selected image to
-    // scramble.
+        var bar = $('#tool-bar').show();
+        var display = bar.find('#timer').find('span');
+
+        var minutes = 0;
+        var seconds = 0;
+        var format = function(value) {
+            var prepend = (value < 10) ? "0" : "";
+            return prepend + value;
+        }
+
+        // When the puzzle is completed, we disable this interval ID
+        window.puzzle.intervalId = setInterval(function() {
+            seconds += 1;
+            if(seconds === 60) {
+                minutes += 1;
+                seconds = 0;
+            }
+            display.text(format(minutes) + ":" + format(seconds));
+        }, 1000);
+    };
+
+    // Do not allow for selection of any other puzzles
+    // In particular, load up the shuffled puzzle into the canvas
+    var initializePuzzle = function(config) {
+        $('#level-select-container').hide();
+        $('#board').css('display', 'block');
+        window.puzzle.load(config);
+    };
+
+    // Allow selection of the puzzle to work with. We simply take the image in
+    // question and remove the other puzzle images in place of a canvas with the
+    // background set to the selected image to scramble.
     $('.puzzle-img').click(function() {
         var that = $(this);
-
-        // Do not allow for selection of any other puzzles
-        $('#level-select-container').hide();
-
-        // Begin starting the timer now that the puzzle has been selected.
-        +function() {
-
-            var bar = $('#tool-bar').show();
-            var display = bar.find('#timer').find('span');
-
-            var minutes = 0;
-            var seconds = 0;
-            var format = function(value) {
-                var prepend = (value < 10) ? "0" : "";
-                return prepend + value;
-            }
-
-            window.puzzle.intervalId = setInterval(function() {
-                seconds += 1;
-                if(seconds === 60) {
-                    minutes += 1;
-                    seconds = 0;
-                }
-                display.text(format(minutes) + ":" + format(seconds));
-            }, 1000);
-
-        }();
+        initializeTimer();
+        initializePuzzle({
+            image: this,
+            difficulty: window.puzzle.Difficulty.EASY
+        });
     });
-
-
 
 });
