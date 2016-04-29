b445b3a3264ca3e533bff0eae71f578aad7a8854
ellenms <shibley@live.unc.edu>
2016-04-28 22:46:19 -0400

    added scoreboard js

--- games/burn/assets/js/puzzle.js
+++ games/burn/assets/js/puzzle.js
@@ -1,4 +1,29 @@
+var score=1000; //the 'high' score, decreases the longer you take
+
 $(function() {
+    
+    //sets up our popup window, credits in magnificPopup files
+    +function() {
+        
+        $('.popup-with-form').magnificPopup({
+         type: 'inline',
+         preloader: false,
+         focus: '#name',
+         closeOnContentClick: false,
+
+         // When elemened is focused, some mobile browsers in some cases zoom in
+         // It looks not nice, so we disable it:
+         callbacks: {
+             beforeOpen: function() {
+                 if($(window).width() < 700) {
+                     this.st.focus = false;
+                 } else {
+                     this.st.focus = '#name';
+                 }
+             }
+          }
+        });
+    
 
     var audioElement = document.createElement('audio');
 
@@ -84,6 +109,7 @@ $(function() {
                 // When the puzzle is completed, we disable this interval ID
                 window.puzzle.intervalId = setInterval(function() {
                     seconds += 1;
+                    score += -1;
                     if(seconds === 60) {
                         minutes += 1;
                         seconds = 0;
@@ -122,7 +148,7 @@ $(function() {
 
     // Preparations for when the puzzle is completed. This allows for selecting certain
     // parts of the image (once the puzzle is constructed) and identifying the problematic
-    // regions of the puzzles
+    // regions of the puzzles. Also launches our scoreboard.
     +function() {
 
         $('.finished-img').click(function(e) {
@@ -142,6 +168,9 @@ $(function() {
             audioElement.addEventListener("load", function() {
                audioElement.play();
             }, true);
+            audioElement.addEventListener('ended', function(){
+                document.getElementById('tableLink').click();
+            });
         });
 
         $('.lamp-correct').click(function(e) {
@@ -152,6 +181,9 @@ $(function() {
             audioElement.addEventListener("load", function() {
                audioElement.play();
             }, true);
+            audioElement.addEventListener('ended', function(){
+                document.getElementById('tableLink').click();
+            });
         });
 
         $('.matches-correct').click(function(e) {
@@ -162,6 +194,9 @@ $(function() {
             audioElement.addEventListener("load", function() {
                audioElement.play();
             }, true);
+            audioElement.addEventListener('ended', function(){
+                document.getElementById('tableLink').click();
+            });            
         });
 
         $('.outlet-correct').click(function(e) {
@@ -172,6 +207,9 @@ $(function() {
             audioElement.addEventListener("load", function() {
                audioElement.play();
             }, true);
+            audioElement.addEventListener('ended', function(){
+                document.getElementById('tableLink').click();
+            });            
         });
 
         $('.stove-correct').click(function(e) {
@@ -182,23 +220,46 @@ $(function() {
             audioElement.addEventListener("load", function() {
                audioElement.play();
             }, true);
+            audioElement.addEventListener('ended', function(){
+                document.getElementById('tableLink').click();
+            });            
         });
 
     }();
+});
 
-/*
-    +function() {
-        //get the cookie for high scores.
-    responseArray = [];
+// Creates our highscore table html to display the top four high scores
+function writeTable() {
+    var responseArray=loadtable();
+    var tbody = $('#body');
+    for (var i = 0; i < 4; i++) {
+        if(responseArray[i]>0){
+            var tr = $('<tr/>').appendTo(tbody);
+            var j=i+1;
+            tr.append('<td class="scorelabel">' + 'High Score '+j+' '+'</td>');
+            tr.append('<td class="scoreval">'+responseArray[i]+'</td>');
+        }
+    }
+    var tr = $('<tr/>').appendTo(tbody);
+    tr.append('<td class="scorelabel">'+'Your Score'+'</td>');
+    tr.append('<td class="scoreval">'+score+'</td>');
+}
 
+// Fetches the high scores from the database, database info in the db-api folder
+function loadtable() {
+    //get the cookie for high scores.
+    
+    var responseArray = [10,10,10,10];
+    
     /* Send a POST request to the high score database
      * Returns a pipe-delimeted string of the top 5 scores (in order)
      * (ex: 2000|1000|750|565|20)
      */
-/*        $.ajax({
+        
+        $.ajax({
             type: 'POST',
-            url: "./db-api/savescores.php",
-            data: "game=fire&score=" + score,
+            url: "savescores.php",
+            data: "game=water&score=" + score,
             dataType: "text",
             success: function(data, status) {
                 response = data;
@@ -212,7 +273,6 @@ $(function() {
         } catch (e) { // Otherwise, follow built-in error handling procedure
             responseArray = [score, -1, -1, -1];
         }
-
         if(responseArray[0] == undefined || responseArray == "NULL") {
             responseArray[0] = score;
         }
@@ -225,46 +285,6 @@ $(function() {
         if(responseArray[3] == undefined || responseArray == "NULL") {
             responseArray[3] = -1;
         }
+        return responseArray;
 
-
-        //VICTORY SCREEN - NEED TO TALK
-        var victoryBg = this.add.sprite(this.game.width, this.game.height, 'victory page bg');
-        victoryBg.x = 0;
-        victoryBg.y = 0;
-        victoryBg.height = this.game.height;
-        victoryBg.width = this.game.width;
-
-        //universal styling
-        textStyle = {
-            font: "48px Arial",
-            fill: "#ffffff",
-            align: "center"*/
-//        };
-
-    //Display the 4 highest scores that were pulled from the database.
-        // If the scores didn't make it to the client for some reason, just display the user's current score.
-        /*scores0 = this.game.add.text(11 * this.game.width / 20, 6.5 * this.game.height / 20, (responseArray[0] + " points").trim(), textStyle);
-        scores0.visible = true;
-        // Load up the high scores, but don't display them yet.
-        scores1 = this.game.add.text(11 * this.game.width / 20, 8 * this.game.height / 20, responseArray[1] + " points", textStyle);
-        scores2 = this.game.add.text(11 * this.game.width / 20, 9.5 * this.game.height / 20, responseArray[2] + " points", textStyle);
-        scores3 = this.game.add.text(11 * this.game.width / 20, 11 * this.game.height / 20, responseArray[3] + " points", textStyle);
-        scores1.visible = false;
-        scores2.visible = false;
-        scores3.visible = false;*/
-
-        //Display the high scores iff they made it back successfully and weren't equal to NULL
-        /*if(responseArray[1] > 0) {
-            scores1.visible = true;
-        }
-        if(responseArray[2] > 0) {
-            scores2.visible = true;
-        }
-        if(responseArray[3] > 0) {
-            scores3.visible = true;
-        }*/
-
-    // Show the user his/her score at the bottom of the results page
-        //this.game.add.text(this.game.width / 2 - 275, 6 * this.game.height / 7, "Your Score:       " + score + " points!", {font: "bold 60px Arial", fill:"#ffffff"})
-
-});
+}
